93b272f26d3d92618e19c73a98fe730b0e9b58fd
Cristian Garner <ceegarner@hotmail.com>
2012-03-05 18:04:04 -0600

    Quienes Somos y Podemos Secciones

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-/* Author:
+/* Author: @Crisgarner
 
 */
 
+///Redirecciones al presionar el li
+$("#quienes_li").click(function(){
+    window.location.href = "quienes.html";
+});
 
+$("#podemos_li").click(function(){
+    window.location.href = "podemos.html";
+});
+
+$("#contacto_li").click(function(){
+    window.location.href = "contacto.html";
+});
 
 
 
