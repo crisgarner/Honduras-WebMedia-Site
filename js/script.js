f030f628631b2e9fc1e1706b58a8889b2defc9d4
Cristian Garner <ceegarner@hotmail.com>
2012-03-18 23:16:11 -0600

    Lightbox and some info

--- js/script.js
+++ js/script.js
@@ -15,5 +15,17 @@ $("#contacto_li").click(function(){
     window.location.href = "contacto.html";
 });
 
+$("#quiero_click").click(function(){
+    window.location.href = "contacto.html";
+});
+
+$('a#video_cromos').click(function(){
+	$.colorbox({href:"http://www.youtube.com/embed/R3JxkfXZPfY"});	
+});
+
+$('a#asi_trabajamos').click(function(){
+	$.colorbox({href:"http://www.youtube.com/embed/R3JxkfXZPfY"});	
+});
+
 
 
