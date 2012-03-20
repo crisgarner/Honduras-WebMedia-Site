9a0aab852469d3e69b54b7e9016bdf8b02eec967
Cristian Garner <ceegarner@hotmail.com>
2012-03-19 19:03:11 -0600

    Contacto form

---
+++ email.php
@@ -0,0 +1,57 @@
+<?php
+if(isset($_POST['email'])) {
+
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "info@honduraswebmedia.com";
+    $email_subject = "Conctacto";
+
+
+    function died($error) {
+        // your error code can go here
+        echo "Lo sentimos pero ha habido un error, nos puedes llamar al 229-21319 o enviarnos un correo a info@honduraswebmedia.com. ";
+        die();
+    }
+
+    // validation expected data exists
+    if(!isset($_POST['nombre']) ||
+        !isset($_POST['email_form']) ||
+        !isset($_POST['mensaje'])) {
+        died('Lo sentimos pero ha habido un error, nos puedes llamar al 229-21319 o enviarnos un correo a info@honduraswebmedia.com.');
+    }
+
+    $first_name = htmlentities($_POST['nombre']); // required
+    $email_from = htmlentities($_POST['email_form']); // required
+    $comments = htmlentities($_POST['mensaje']); // required
+    $telephone = htmlentities($_POST['telefono_form']);
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'Email Invalido.<br />';
+  }
+    $string_exp = "/^[A-Za-z .'-]+$/";
+  if(!preg_match($string_exp,$first_name)) {
+    $error_message .= 'Nombre no valido<br />';
+  }
+
+
+    $email_message = "Detalle del contacto.\n\n";
+
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+
+    $email_message .= "Nombre: ".clean_string($first_name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Telephone: ".clean_string($telephone)."\n";
+    $email_message .= "Comments: ".clean_string($comments)."\n";
+
+
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+}
+?>
--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
 
 */
 
+// Create a jQuery exists method
+jQuery.fn.exists = function () { return jQuery(this).length > 0; }
+
 ///Redirecciones al presionar el li
 $("#quienes_li").click(function(){
     window.location.href = "quienes.html";
@@ -19,13 +22,80 @@ $("#quiero_click").click(function(){
     window.location.href = "contacto.html";
 });
 
+//Videos
 $('a#video_cromos').click(function(){
-	$.colorbox({href:"http://www.youtube.com/embed/R3JxkfXZPfY"});	
+	$.colorbox({html:'<iframe width="560" height="315" src="http://www.youtube.com/embed/R3JxkfXZPfY" frameborder="0" allowfullscreen></iframe>'});
 });
 
 $('a#asi_trabajamos').click(function(){
-	$.colorbox({href:"http://www.youtube.com/embed/R3JxkfXZPfY"});	
+	$.colorbox({html:'<iframe width="560" height="315" src="http://www.youtube.com/embed/R3JxkfXZPfY" frameborder="0" allowfullscreen></iframe>'});
 });
 
+//validacion Formulario
+$(document).ready(function() {
+    if ($("#form_contacto").exists()) {
+        $.validity.setup({ outputMode:"label" })
 
+        $("#form_contacto").validity(function(){
 
+            if (validateMyAjaxInputs()) {
+                // Do ajax request here
+                var nombre = $("#nombre").val();
+                var email = $("#email_form").val();
+                var empresa = $("#empresa").val();
+                var telefono = $("#telefono_form").val();
+                var mensaje = $("#mensaje").val();
+                var datastr = "nombre="+nombre+ "&email_form="+ email + "&empresa=" + empresa+ "&telefono="+ telefono+"&mensaje="+ mensaje  ;
+                $.ajax({
+                    type: "POST",
+                    url: "email.php",
+                    data: datastr,
+                    cache: false,
+                    success: function(html){
+                    $("#formulario").fadeOut(0);
+                    $("#formulario").html("<span id='gracias'>Gracias contactarnos, nos comunicaremos contigo lo mas pronto posible.</span>");
+                    $("#formulario").addClass("gracias");
+                    $("#formulario").fadeIn("slow");
+                    }
+                });
+            }
+        });
+    }
+
+});
+
+function validateMyAjaxInputs() {
+    $("label.error").remove();
+    $.validity.start();
+    $("#nombre").require("Ingresa tu nombre.");
+    $("#email_form").match('email',"Correo no valido.");
+    $("#email_form").require("Ingresa tu Correo.");
+    var result = $.validity.end();
+    return result.valid;
+}
+
+$('label.error[for="nombre"]').live("click",function(){
+    $(this).fadeOut(500);
+});
+
+$('#nombre').live("click",function(){
+    $('label.error[for="nombre"]').fadeOut(500);
+});
+
+$('#email_form').live("click",function(){
+    $('label.error[for="email_form"]').fadeOut(500);
+});
+
+$('label.error[for="email_form"]').live("click",function(){
+    $(this).fadeOut(500);
+});
+
+$(".validity-tooltip").live("click",function(){
+    $(".validity-tooltip").fadeOut(500);
+});
+
+$("#form_contacto").submit(function(e){
+
+    return false;
+
+});
